48b76afed34afa76bdb8733cfa4f720c020c9853
Kamil Nowak <kamnowax@gmail.com>
2026-04-25 15:52:19 +0200

    Add meeting

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -2,16 +2,13 @@ package com.company.enroller.controllers;
 
 import java.util.Collection;
 
-import com.company.enroller.model.Participant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 import com.company.enroller.model.Meeting;
 import com.company.enroller.persistence.MeetingService;
-
 
 @RestController
 @RequestMapping("/meetings")
@@ -20,28 +17,39 @@ public class MeetingRestController {
     @Autowired
     MeetingService meetingService;
 
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public ResponseEntity<?> getMeetings() {
-        Collection<Meeting> meetings = meetingService.getAll();
-        return new ResponseEntity<>(meetings, HttpStatus.OK);
+    @GetMapping
+    public ResponseEntity<Collection<Meeting>> getMeetings() {
+        return ResponseEntity.ok(meetingService.getAll());
     }
+
     @GetMapping("/{id}")
-    public ResponseEntity<?> getMeeting(@PathVariable("id") long id) {
+    public ResponseEntity<Meeting> getMeeting(@PathVariable Long id) {
 
         Meeting meeting = meetingService.findById(id);
 
         if (meeting == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return ResponseEntity.notFound().build();
         }
 
-        return new ResponseEntity<>(meeting, HttpStatus.OK);
+        return ResponseEntity.ok(meeting);
     }
-//    @RequestMapping(value = "", method = RequestMethod.POST)
-//    public ResponseEntity<?> registerMeeting(@RequestBody Meeting meeting) {
-//        Meeting foundMeeting = meetingService.findByLogin(participant.getLogin());
-//        if (foundParticipant != null) {
-//            return new ResponseEntity<>("Already registered", HttpStatus.CONFLICT);
-//        }
-//        participantService.add(participant);
-//        return new ResponseEntity<>(participant, HttpStatus.CREATED);    }
+
+    @PostMapping
+    public ResponseEntity<?> registerMeeting(@RequestBody Meeting meeting) {
+
+        // sprawdz czy istnieje
+        if (meeting.getId() != 0) {
+            Meeting foundMeeting = meetingService.findById(meeting.getId());
+
+            if (foundMeeting != null) {
+                return ResponseEntity.status(HttpStatus.CONFLICT)
+                        .body("Already exists");
+            }
+        }
+
+        meetingService.add(meeting);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(meeting);
+    }
 }
--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -2,6 +2,8 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import com.company.enroller.model.Participant;
+import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
@@ -11,22 +13,29 @@ import com.company.enroller.model.Meeting;
 @Component("meetingService")
 public class MeetingService {
 
-	DatabaseConnector connector;
+    DatabaseConnector connector;
 
-	public MeetingService() {
-		connector = DatabaseConnector.getInstance();
-	}
-
-	public Collection<Meeting> getAll() {
-		String hql = "FROM Meeting";
-		Query query = connector.getSession().createQuery(hql);
-		return query.list();
-	}
-    public Meeting findById(long id) {
-        String hql = "FROM Meeting WHERE id = :id";
-        Query query = connector.getSession().createQuery(hql);
-        query.setParameter("id", id);
-        return (Meeting) query.uniqueResult();
+    public MeetingService() {
+        connector = DatabaseConnector.getInstance();
     }
 
+    public Collection<Meeting> getAll() {
+        Session session = connector.getSession();
+
+        Query<Meeting> query = session.createQuery("FROM Meeting", Meeting.class);
+
+        return query.list();
+    }
+
+    public Meeting findById(Long id) {
+        Session session = connector.getSession();
+
+        return session.get(Meeting.class, id);
+    }
+
+    public void add(Meeting meeting) {
+        Transaction transaction = connector.getSession().beginTransaction();
+        connector.getSession().save(meeting);
+        transaction.commit();
+    }
 }
